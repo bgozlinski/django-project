de568d23d5b1a5f0dda502954e4bc0dcb4c78304
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-19 15:10:48 +0200

    labels

--- cars/urls.py
+++ cars/urls.py
@@ -4,9 +4,9 @@ from cars.views import cars_response, cars_add, cars_edit, cars_del
 
 
 urlpatterns = [
-    path('all/', cars_response),
-    path('add/', cars_add),
-    path('edit/<int:id>', cars_edit),
-    path('del/<int:id>', cars_del),
+    path('all/', cars_response, name='all'),
+    path('add/', cars_add, name='add'),
+    path('edit/<int:id>', cars_edit, name='edit'),
+    path('del/<int:id>', cars_del, name='del'),
 ]
 
