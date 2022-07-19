ac8abc142e1b8f95f62b4064a4348bcaa5ecd392
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-19 22:21:18 +0200

    login and logout

--- cars/urls.py
+++ cars/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from cars.views import cars_response, cars_add, cars_edit, cars_del
+from cars.views import cars_response, cars_add, cars_edit, cars_del #  importuj widoki
 
-
-
+# cars urls
 urlpatterns = [
     path('all/', cars_response, name='all'),
     path('add/', cars_add, name='add'),
     path('edit/<int:id>', cars_edit, name='edit'),
     path('del/<int:id>', cars_del, name='del'),
-]
 
+]
--- django_project/settings.py
+++ django_project/settings.py
@@ -112,3 +112,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#  login urls
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'all'
+
+#  logout urls
+LOGOUT_REDIRECT_URL = 'login'
